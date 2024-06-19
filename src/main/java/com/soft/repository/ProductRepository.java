b81ec8c0ba47eabb6b9ae02723190002aa759ab6
Pham Nhu Son <phamnhusondolla@gmail.com>
2024-06-19 10:54:05 +0700

    Them chuc nang tim kiem phan trang san pham

--- src/main/java/com/soft/repository/ProductRepository.java
+++ src/main/java/com/soft/repository/ProductRepository.java
@@ -1,10 +1,15 @@
 package com.soft.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.soft.models.Product;
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer>{
 
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+	@Query("SELECT p FROM Product p where p.productName LIKE %?1%")
+	List<Product> searchProduct(String keyword);
 }
--- src/main/java/com/soft/service/ProductService.java
+++ src/main/java/com/soft/service/ProductService.java
@@ -2,6 +2,8 @@ package com.soft.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+
 import com.soft.models.Product;
 
 public interface ProductService {
@@ -14,4 +16,9 @@ public interface ProductService {
 	Boolean update(Product product);
 
 	Boolean delete(Integer id);
+	
+	List<Product> searchProduct(String keyword);
+	
+	Page<Product> getAll(Integer pageNo);
+	Page<Product> searchProduct(String keyword , Integer pageNo);
 }
